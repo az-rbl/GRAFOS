da384875a4de8410e3a3306c0206c14167515025
raul <192793@iberopuebla.mx>
2023-03-11 12:48:53 -0600

    cambios al 12

--- Asignacion_12.py
+++ Asignacion_12.py
@@ -1,20 +1,26 @@
+from Asignacion_8_1 import leer_grafo as lg
 def initialize_SS(G,s):
     d = [1000]*len(G)
     pi = [None]*len(G)
     d[s]=0
     return d,pi
-def extract_min(q):
+def extract_min(Q,G,d):
     pass
-def relax(u,v,w):
+def relax(u,v,G,d):
     pass
 
-def dijkstra(G,w,s):
-    d,g=initialize_SS(G,s)
+def dijkstra(G,s):
+    d,pi=initialize_SS(G,s)
     S=[]
     Q=G
     while(Q!=[]):
-        u =extract_min(Q)
-        S=u
+        u =extract_min(Q,G,d)
+        S.append(u)
         for v in S:
-            relax(u,v,w)
+            relax(u,v,G,d)
+
+if __name__=="__main__":
+    G=lg('ejemplo1.txt')
+    d,pi=initialize_SS(G,0)
+    print(d,pi)
     
