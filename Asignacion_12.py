60c0aff7cdc74eabd29a3e97f1e7dacefeca8421
raul <192793@iberopuebla.mx>
2023-03-20 12:56:11 -0600

    Finalizado

--- Asignacion_12.py
+++ Asignacion_12.py
@@ -10,7 +10,7 @@ def initialize_SS(G,s):
 
 #funcion que regresa el valor de peso mínimo de entre los nodos de la cola Q
 def extract_min(Q,G,d):
-    min_w = float('inf')
+    min_w = 1000
     for u in Q:
         if d[u] < min_w:
             min_w = d[u]
@@ -21,17 +21,18 @@ def w(u,v,G):
     #print(list(range(0,len(G[u]),2)))
     for i in list(range(0,len(G[u]),2)):
         #print(i)
+        #print(f"{G[u][i]}=={v}")
         if G[u][i]==v:
             return G[u][i+1]
-    
-    return -1
+    return 1000
+    #return -1
 
 
 def relax(u,v,G,d, pi):
-    if w(u,v,G)>0:
-        if d[u] > d[v] +w(u,v,G):
-            d[u]=d[v]+w(u,v,G)
-            pi[u]= v
+    #if w(u,v,G)>0:
+    if d[v] > d[u] +w(u,v,G):
+        d[v]=d[u]+w(u,v,G)
+        pi[v]= u
 
 
 def dijkstra(G,s):
@@ -41,8 +42,9 @@ def dijkstra(G,s):
     while(Q!=[]):
         u =extract_min(Q,G,d)
         S.append(u)
-        for v in S:
+        for v in Q:
             relax(u,v,G,d, pi)
+        #print(d,pi, S, Q)
     return d,pi, S, Q
 
 
@@ -56,27 +58,29 @@ def rutas(pi, i, l):
 def todos_nodos(g):
     r =[]
     ds=[]
+    pis= []
     for i in range(len(g)):
         nl=[]
         r.append(nl)
         #print(f"padre {i}")
         d,pi, S, Q=dijkstra(g,i)
+        pis.append(pi)
         for j in range(len(pi)):
             l=[]
             rut =rutas(pi,j,l)
             r[i].append(rut)
         ds.append(d)
-    return r,ds
+    return r,ds,pis
 
 def dibujar(d,pi):
     
     G = nx.DiGraph()
     for j in range(len(d)):
-        G.add_node(j, d=d[j])
+        G.add_node((j,d[j]))
     
     for i in range(len(pi)):
         if pi[i] != None:
-            G.add_edge(pi[i],i)
+            G.add_edge((pi[i],d[pi[i]]),(i,d[i]))
     #color_map =[color]
     #color_map =[node_color for node in G]
     #nx.draw(G, with_labels=True, node_color=colors, font_color= 'yellow')
@@ -84,21 +88,27 @@ def dibujar(d,pi):
     #plt.show()
     return G
 
-
+def graficar(ds,pis):
+    fig, all_axes = plt.subplots(2, 2)
+    ax = all_axes.flat
+    for i in range(4):
+         nx.draw(dibujar(ds[i],pis[i]), with_labels=True,  arrows=True,node_color='pink', ax=ax[i])
+         ax[i].set_title(f"Arbol con origen en {i}")
+    plt.show()
 
 if __name__=="__main__":
     G=lg('ejemplo1.txt')
     print(G)
-    # r,d=todos_nodos(G)
-    # print(f"rutas: {r}")
-    # print(f"distancias: {d}")
-    d,pi, S, Q=dijkstra(G,5)
-    print(d,pi,S,Q)
-   
+    r,d, pi=todos_nodos(G)
+    print(f"rutas: {r}")
+    print(f"distancias: {d}")
+    # d,pi, S, Q=dijkstra(G,5)
+    # print(d,pi,S,Q)
+    graficar(d,pi)
 
-    Gf=dibujar(d,pi)
-    nx.draw(Gf, with_labels=True,  arrows=True)
-    pos = nx.spring_layout(Gf)
-    labels = dict(enumerate(d))
-    nx.draw_networkx_labels(Gf,pos,labels)
-    plt.show()
+    # Gf=dibujar(d,pi)
+    # nx.draw(Gf, with_labels=True,  arrows=True,node_color='pink')
+    #pos = nx.spring_layout(Gf)
+    #labels = dict(enumerate(d))
+    #nx.draw_networkx_labels(Gf,pos,labels)
+    # plt.show()
